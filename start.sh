f81f854470e502fa5494afd58820e3ce51db04fb
Michal Charemza <michal@charemza.name>
2025-02-25 21:38:44 +0000

    infra: use nginx to serve static files (#61)
    Also avoids running the server as root

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@
 set -e
 
 python manage.py collectstatic
-exec python manage.py runserver 0.0.0.0:8000
+
+exec parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
+    "python manage.py runserver 0.0.0.0:8001" \
+    "nginx -p /home/scl"
