4c2e16f609e8a25d725c413863d08bf9ebc005f1
Michal Charemza <michal@charemza.name>
2025-02-25 11:04:04 +0000

    infra: enforce a specific HTTP header and value from CloudFront (#52)
    To make sure that only _our_ CloudFront distribution can communicate
    with the ALB, as AWS recommend now enforce a HTTP header check.

--- infra/Ω-output.tf
+++ infra/Ω-output.tf
@@ -1,3 +1,7 @@
 output "lb_alias" {
   value = "You must make sure that '${var.domain_name}' has an ALIAS record (or similar) pointing to '${aws_lb.main.dns_name}'"
 }
+
+output "cdn_header" {
+  value = "You must make sure that CloudFront sets the '${random_id.cdn_header_name.hex}' HTTP header to '${random_bytes.cdn_header_value.hex}' (without quotes)"
+}
