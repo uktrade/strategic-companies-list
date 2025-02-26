e222468b0dd83afb49debc53359c7feb434f3e2d
Michal Charemza <michal@charemza.name>
2025-02-26 12:02:15 +0000

    infra: log to console, even when DEBUG off

--- scl/settings.py
+++ scl/settings.py
@@ -135,3 +135,20 @@ STATIC_URL = 'assets/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
