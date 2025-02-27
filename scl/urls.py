792b70111f52e1152844d999e31f6c4e1e753d4e
Ian leggett <>
2025-02-27 09:02:17 +0000

    feat: add company briefing page

--- scl/urls.py
+++ scl/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from scl.core.views import index, engagement, aws_credentials
+from scl.core.views import index, company
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
     path("engagement", engagement),
+    path("company-briefing", company, name='company-briefing'),
     path("api/v1/aws-credentials", aws_credentials),
 ]
