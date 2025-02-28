bc4f2f2dd4d2c9054e26b0218f7c8ff2cdf24ee3
Michal Charemza <michal@charemza.name>
2025-02-28 15:54:32 +0000

    infra: move to more of a production ready server
    Technically uvicorn is more for async apps, and we have no async views, but we
    might have them, or have more live-views Websocket-y things.

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 set -e
 
 exec parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
-    "python manage.py runserver 0.0.0.0:8001" \
+    "python -m uvicorn --host 0.0.0.0 --port 8001 scl.asgi:application" \
     "nginx -p /home/scl"
