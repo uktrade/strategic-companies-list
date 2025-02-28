7db4c0dd9c328abf45d139d1c98a512f9c859b19
Michal Charemza <michal@charemza.name>
2025-02-28 04:54:02 +0000

    infra: combine files
    The pattern I'm going for, trying to make it somehow neat/structured, is that
    files can only depend on files "above" them

--- infra/7-ecs-and-transcribe.tf
+++ infra/7-ecs-and-transcribe.tf
@@ -198,3 +198,38 @@ resource "aws_vpc_security_group_egress_rule" "ecs_service_https_to_all" {
   from_port   = "443"
   to_port     = "443"
 }
+
+resource "aws_iam_role" "transcribe" {
+  name = "${var.prefix}-transcrbe-${var.suffix}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.ecs_task_main_service.arn
+        },
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "transcribe" {
+  name = "${var.prefix}-transcribe-${var.suffix}"
+  role = aws_iam_role.transcribe.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "transcribe:StartStreamTranscriptionWebSocket",
+        ],
+        Resource = "*"
+      },
+    ]
+  })
+}
