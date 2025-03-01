1ce46b78e887e0f3e8b359c8a2791516fefff345
Michal Charemza <michal@charemza.name>
2025-03-01 07:04:04 +0000

    chore: cleanup unused files in 'static' app (#121)

---
+++ scl/static/__init__.py
@@ -0,0 +1,3 @@
+# This app only exists so we can have a local volume replacing _some_ of the static assets, but
+# not the ones in this app. The ones in this app are added by the Dockerfile (pulled by npm and/or
+# processed by Webpack)
