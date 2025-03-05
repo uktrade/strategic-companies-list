b02dfc8c9546680d907a8350011be23dfab189df
Michal Charemza <michal@charemza.name>
2025-03-05 17:06:16 +0000

    feat: save version when editing a company

--- scl/core/api/v1/company.py
+++ scl/core/api/v1/company.py
@@ -1,15 +1,26 @@
 import json
-from scl.core.models import Company
+
+import reversion
 from django.http import JsonResponse
+
+from scl.core.models import Company
 
 
 def company_api(request, duns_number):
     data = json.loads(request.body)
     company = Company.objects.get(duns_number=duns_number)
+
     if request.method == 'PATCH':
-        company.hmg_priorities = data.get('hmg_priorities')
-        company.company_priorities = data.get('company_priorities')
-        company.save()
+        with reversion.create_revision():
+            company.hmg_priorities = data.get('hmg_priorities')
+            company.company_priorities = data.get('company_priorities')
+            company.save()
+
+            reversion.set_user(request.user)
+            reversion.set_comment(
+                "Updated hmg_priorities and company_priorities via API "
+                f"({request.build_absolute_uri()} from {request.headers['referer']})"
+            )
 
     return JsonResponse(
         {
--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ done
 echo "PostrgreSQL ready"
 
 python manage.py migrate
+python manage.py createinitialrevisions
 
 exec parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
     "python -m uvicorn --host 0.0.0.0 --port 8001 scl.asgi:application" \
