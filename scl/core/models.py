6959e212a9f89cf3e7971ca30ff325d4aa98bb12
Ian leggett <>
2025-03-05 08:34:22 +0000

    feat: add company model

--- scl/core/models.py
+++ scl/core/models.py
@@ -1,5 +1,21 @@
+import uuid
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
+
+# Create your models here.
+
+
+class Company(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(blank=False, null=False, max_length=128)
+    key_facts = models.TextField(blank=False, null=False)
+    issues = models.TextField(blank=False, null=True)
+    priorities = models.TextField(blank=False, null=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
