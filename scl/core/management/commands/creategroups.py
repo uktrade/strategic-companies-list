35984f7985ef4e23c14c479b4ff15516c103ef7b
Michal Charemza <michal@charemza.name>
2025-03-07 15:40:34 +0000

    feat: enforce group membership to get access
    This is so permissions can be controlled within the app itself

--- scl/core/management/commands/creategroups.py
+++ scl/core/management/commands/creategroups.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 
@@ -6,8 +7,7 @@ class Command(BaseCommand):
     help = "Ensures all groups are created"
 
     def handle(self, *args, **options):
-    	Group.objects.get_or_create(name='Basic access')
-    	self.stdout.write(
+        Group.objects.get_or_create(name=settings.BASIC_ACCESS_GROUP)
+        self.stdout.write(
             self.style.SUCCESS("Successfully created the 'Basic access' group")
         )
- 
--- scl/core/middleware.py
+++ scl/core/middleware.py
@@ -39,3 +39,21 @@ def IPFilterMiddleware(get_response):
             }, status=403)
 
     return middleware
+
+
+def BasicAccessMiddleware(get_response):
+    group_name = settings.BASIC_ACCESS_GROUP
+    exclude_paths = getattr(settings, 'BASIC_ACCESS_EXCLUDE_PATHS', [])
+
+    def middleware(request):
+        allowed = request.path in exclude_paths or request.user.groups.filter(name=group_name).exists()
+
+        if allowed:
+            return get_response(request)
+        else:
+            return render(request, '403_basic_access.html', {
+                'email_address': request.user.email,
+                'user_id': request.user.username,
+            }, status=403)
+
+    return middleware
