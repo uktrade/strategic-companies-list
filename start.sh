bbe55309d38dea75d5c51d5660f910e9ea694e55
Michal Charemza <michal@charemza.name>
2025-03-07 13:14:59 +0000

    feat: add basic access group
    The start of more of a permissions model in the app

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ echo "PostrgreSQL ready"
 
 python manage.py migrate
 python manage.py createinitialrevisions
+python manage.py creategroups
 
 exec parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
     "python -m uvicorn --host 0.0.0.0 --port 8001 scl.asgi:application" \
