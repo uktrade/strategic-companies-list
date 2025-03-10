740b1438392cc5bbc014377bdc5a7c8fd839d505
Ian Leggett <contactleggett@gmail.com>
2025-03-10 13:14:47 +0000

    feat: add company engagements page (#259)
    This change adds a company engagements page that lists all past and
    present engagements.
    
    ---------
    
    Co-authored-by: Ian leggett <>

--- scl/core/views/html.py
+++ scl/core/views/html.py
@@ -3,6 +3,7 @@ from datetime import date
 
 from django.shortcuts import render, redirect
 from reversion.models import Version
+from django.http import JsonResponse
 
 from scl.core.forms import EngagementForm
 from scl.core.models import Company, Engagement
@@ -16,7 +17,7 @@ def index(request):
     all_companies = list(Company.objects.all())
     your_companies = list(request.user.managed_companies.all())
     your_future_enagements = list(Engagement.objects.filter(
-        company__in=your_companies, date__gte=today))
+        company__in=your_companies, date__gte=today).order_by('-date'))
 
     return render(request, "index.html", {
         "all_companies": all_companies,
@@ -30,7 +31,8 @@ def company_briefing(request, duns_number):
 
     company = Company.objects.get(duns_number=duns_number)
     account_managers = list(company.account_manager.all())
-    past_engagements = list(company.engagements.filter(date__lte=today))
+    past_engagements = list(company.engagements.filter(
+        date__lte=today).order_by('-date'))[0:4]
     is_privileged = request.user in account_managers
 
     versions = Version.objects.get_for_object(company)
@@ -51,7 +53,7 @@ def engagement(request, engagement_id):
 
     is_privileged = request.user in engagement.company.account_manager.all()
     if not is_privileged:
-        return JsonReponse(403, {})
+        return JsonResponse(403, safe=False)
 
     versions = Version.objects.get_for_object(engagement)
     engagement_first_version = versions.last()
@@ -65,12 +67,38 @@ def engagement(request, engagement_id):
     return render(request, "engagements.html", {
         "engagement": engagement,
         "engagement_first_version": engagement_first_version,
-        "notes_versions": notes_versions
+        "notes_versions": notes_versions,
+    })
+
+
+def engagements(request, duns_number):
+    today = date.today()
+
+    company = Company.objects.get(duns_number=duns_number)
+    past_engagements = list(company.engagements.filter(
+        date__lte=today).order_by('-date'))[1:]
+    engagements = list(company.engagements.filter(
+        date__gte=today).order_by('-date'))
+
+    is_privileged = request.user in company.account_manager.all()
+    if not is_privileged:
+        return JsonResponse(403, safe=False)
+
+    return render(request, "all_engagements.html", {
+        "company": company,
+        "engagements": engagements,
+        "past_engagements": past_engagements,
+        "is_privileged": is_privileged,
     })
 
 
 def engagement_add(request, duns_number):
     company = Company.objects.get(duns_number=duns_number)
+    is_privileged = request.user in company.account_manager.all()
+
+    if not is_privileged:
+        return JsonResponse(403, safe=False)
+
     if request.method == 'POST':
         form = EngagementForm(request.POST)
         if form.is_valid():
