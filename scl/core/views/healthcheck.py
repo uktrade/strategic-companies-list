461c3129e984154122b253e8a77004b7afd5d2b3
Ian leggett <>
2025-03-28 09:17:01 +0000

    implement pingdom healthcheck

--- scl/core/views/healthcheck.py
+++ scl/core/views/healthcheck.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse, JsonResponse
 
+
 def lb_healthcheck(request):
     return HttpResponse("OK")
+
+
+def healthcheck(request):
+    return HttpResponse('<?xml version="1.0" encoding="UTF-8"?><pingdom_http_custom_check><status>OK</status><response_time>500</response_time></pingdom_http_custom_check>', content_type="text/xml")
