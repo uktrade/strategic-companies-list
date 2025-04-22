aa1f65b3e6f1b84b0a85c68b818402b3d5906a36
Paul Gain <paul.gain@digital.trade.gov.uk>
2025-04-22 19:09:18 +0100

    Wire up the assertions

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import '@testing-library/cypress/add-commands';
+import './commands';
+import './assertions';
