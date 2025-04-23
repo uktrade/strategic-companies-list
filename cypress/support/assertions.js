703bddf74a182b310f6eff1c25f0a4591fef3524
Paul Gain <paul.gain@digital.trade.gov.uk>
2025-04-23 10:19:34 +0100

    Add command docs strings

--- cypress/support/assertions.js
+++ cypress/support/assertions.js
@@ -1,5 +1,53 @@
 import "@testing-library/cypress/add-commands";
 
+/**
+ * Asserts that a company briefing section exists with specified content and elements
+ *
+ * This command finds a section heading with the provided text and verifies various
+ * elements within its parent container based on the provided options.
+ *
+ * @param {string} headingText - The text of the heading that identifies the briefing section
+ * @param {Object} options - Configuration options for the assertion
+ * @param {(string|string[])} [options.content] - The content to verify within the section
+ *    If an array is provided, it checks for each string in a list element
+ *    If a string is provided, it checks for the content as direct text
+ * @param {string} [options.buttonName] - The name of a button to verify within the section
+ * @param {Array<{text: string, href: string}>} [options.links] - Array of link objects to verify
+ *    Each link object should have 'text' and 'href' properties
+ * @param {boolean} [options.hasPrivilegedTag] - Whether to check for a red "PRIVILEGED" tag
+ *
+ * @example
+ * // Assert a basic company briefing section with heading
+ * cy.assertCompanyBriefingSection("Testing Corp");
+ *
+ * @example
+ * // Assert a section with content text
+ * cy.assertCompanyBriefingSection("Summary", {
+ *   content: "Currently this company has no summary."
+ * });
+ *
+ * @example
+ * // Assert a section with list items
+ * cy.assertCompanyBriefingSection("Key Products", {
+ *   content: [
+ *      "Headquartered in Canada",
+ *      "Has a global turnover of $8000000",
+ *      "Employs 6,000 people globally"
+ *   ]
+ * });
+ *
+ * @example
+ * // Assert a section with links
+ * cy.assertCompanyBriefingSection("SCIT administrators", {
+ *   content: "Vyvyan Holland",
+ *   links: [
+ *     text: "local.user@businessandtrade.gov.uk",
+ *     href: "mailto:local.user@businessandtrade.gov.uk",
+ *   ],
+ *   hasPrivilegedTag: true
+ * });
+ */
+
 Cypress.Commands.add(
   "assertCompanyBriefingSection",
   (headingText, options = {}) => {
