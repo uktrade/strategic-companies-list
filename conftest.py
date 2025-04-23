9cc54b05773b098ac3aabcf3608507da9010efcc
Edie Pearce <e.pearce@tutanota.com>
2025-04-23 11:46:16 +0100

    SCL-97: Test key people api (#356)

--- conftest.py
+++ conftest.py
@@ -50,7 +50,10 @@ def viewer_user_client(client, viewer_user):
 
 @pytest.fixture
 @pytest.mark.django_db
-def company(viewer_user):
+def company_acc_manager(viewer_user):
+    """
+    Company with the viewer_user as the account manager.
+    """
     with reversion.create_revision():
         company = factories.CompanyFactory()
 
@@ -76,6 +79,37 @@ def company(viewer_user):
         )
         factories.EngagementNoteFactory.create_batch(
             2, engagement=engagements[1], created_by=viewer_user
+        )
+
+    return company
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def company_not_acc_manager(viewer_user):
+    """
+    Company with an account manager that is not the viewer_user.
+    """
+    with reversion.create_revision():
+        company = factories.CompanyFactory()
+
+        reversion.set_user(viewer_user)
+
+        factories.CompanyAccountManagerFactory.create(company=company, is_lead=True)
+        acc_manager = factories.CompanyAccountManagerFactory.create(company=company)
+
+        factories.KeyPeopleFactory.create_batch(3, company=company)
+
+        factories.InsightFactory.create_batch(
+            5, company=company, insight_type="company_priority"
+        )
+        factories.InsightFactory.create_batch(
+            6, company=company, insight_type="hmg_priority"
+        )
+
+        engagements = factories.EngagementFactory.create_batch(4, company=company)
+        factories.EngagementNoteFactory.create_batch(
+            3, engagement=engagements[0], created_by=acc_manager.account_manager
         )
 
     return company
