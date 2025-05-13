9b6bd5f62472b28bf831ec91b7cbb62cfde8d477
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-05-13 11:11:55 +0100

    testfornewformat

--- cypress/e2e/key-people.cy.js
+++ cypress/e2e/key-people.cy.js
@@ -17,34 +17,34 @@ describe("Company Details page", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Add people");
     cy.findByRole("textbox", {
+      name: /role/i,
+    })
+      .clear()
+      .type("Company Rep");
+    cy.findByRole("textbox", {
       name: /name/i,
     })
       .clear()
       .type("Test Person");
-    cy.findByRole("textbox", {
-      name: /role/i,
-    })
-      .clear()
-      .type("Dogsbody");
     cy.findByRole("textbox", {
       name: /email/i,
     })
       .clear()
       .type("test.person@company.co.uk");
     cy.clickButton("Save");
-    cy.get("section").contains("Dogsbody: Test Person");
+    cy.get("section").contains("Company Rep - Test Person (test.person@company.co.uk)");
   });
 
   it("should not save a Key Person if you cancel", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Add people");
     cy.findByRole("textbox", {
-      name: /name/i,
+      name: /role/i,
     })
       .clear()
       .type("Shouldn't Exist");
     cy.findByRole("textbox", {
-      name: /role/i,
+      name: /name/i,
     })
       .clear()
       .type("Shouldn't Exist");
@@ -55,30 +55,29 @@ describe("Company Details page", () => {
       .type("shouldnt.exist@company.co.uk");
     cy.get("#cancel-add-key-person").click();
     cy.get("section")
-      .contains("Shouldn't Exist: Shouldn't Exist")
+      .contains("Shouldn't Exist - Shouldn't Exist")
       .should("not.exist");
   });
 
   it("should allow you to edit a Key Person", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Edit people");
-
-    cy.get("#people\\.0\\.name").clear().type("Test Person 2");
     cy.get("#people\\.0\\.role").clear().type("Company Rep");
+    cy.get("#people\\.0\\.name").clear().type("Test Person 2");
     cy.get("#people\\.0\\.email").clear().type("test.person@company.co.uk");
     cy.clickButton("Save");
-    cy.get("section").contains("Company Rep: Test Person 2");
+    cy.get("section").contains("Company Rep - Test Person 2");
   });
 
   it("should not allow you to edit a Key Person if you cancel", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Edit people");
-    cy.get("#people\\.0\\.name").clear().type("Shouldn't Exist");
     cy.get("#people\\.0\\.role").clear().type("Shouldn't Exist");
+    cy.get("#people\\.0\\.name").clear().type("Shouldn't Exist");
     cy.get("#people\\.0\\.email").clear().type("shouldnt.exist@company.co.uk");
     cy.get("#cancel-edit-key-person").click();
     cy.get("section")
-      .contains("Shouldn't Exist: Shouldn't Exist")
+      .contains("Shouldn't Exist - Shouldn't Exist")
       .should("not.exist");
   });
 
@@ -90,15 +89,15 @@ describe("Company Details page", () => {
     cy.get(".govuk-error-message").contains("Role is required");
     cy.get(".govuk-error-message").contains("Email is required");
     cy.findByRole("textbox", {
-      name: /name/i,
+      name: /role/i,
     })
       .clear()
       .type("Test Person");
     cy.clickButton("Save");
-    cy.get(".govuk-error-message").contains("Role is required");
+    cy.get(".govuk-error-message").contains("Name is required");
     cy.get(".govuk-error-message").contains("Email is required");
     cy.findByRole("textbox", {
-      name: /role/i,
+      name: /name/i,
     })
       .clear()
       .type("Test Role");
@@ -109,18 +108,18 @@ describe("Company Details page", () => {
   it("should stop you from editing a Key Person if you are missing fields", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Edit people");
-    cy.get("#people\\.0\\.name").clear();
     cy.get("#people\\.0\\.role").clear();
+    cy.get("#people\\.0\\.name").clear();
     cy.get("#people\\.0\\.email").clear();
     cy.clickButton("Save");
     cy.get(".govuk-error-message").contains("Name is required");
     cy.get(".govuk-error-message").contains("Role is required");
     cy.get(".govuk-error-message").contains("Email is required");
-    cy.get("#people\\.0\\.name").clear().type("Test Person");
-    cy.clickButton("Save");
-    cy.get(".govuk-error-message").contains("Role is required");
-    cy.get(".govuk-error-message").contains("Email is required");
     cy.get("#people\\.0\\.role").clear().type("Test Role");
+    cy.clickButton("Save");
+    cy.get(".govuk-error-message").contains("Name is required");
+    cy.get(".govuk-error-message").contains("Email is required");
+    cy.get("#people\\.0\\.name").clear().type("Test Name");
     cy.clickButton("Save");
     cy.get(".govuk-error-message").contains("Email is required");
   });
@@ -129,6 +128,8 @@ describe("Company Details page", () => {
     cy.visit(`/company-briefing/${company.duns_number}`);
     cy.clickButton("Edit people");
     cy.contains("Delete").click();
-    cy.get("section").contains("Test Person").should("not.exist");
+    cy.get("section")
+      .contains("Test Person")
+      .should("not.exist");
   });
 });
