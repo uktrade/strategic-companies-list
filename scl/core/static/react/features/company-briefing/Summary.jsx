370f27fe9db2c46c5068ab9d437704ff18f94122
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-05-16 15:20:36 +0100

    removesummarypatchapi

--- scl/core/static/react/features/company-briefing/Summary.jsx
+++ scl/core/static/react/features/company-briefing/Summary.jsx
@@ -1,48 +1,14 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 
 import Section from "../../components/Section";
-import Update from "../../forms/summary/Update";
-import ApiProxy from "../../proxy";
 import LoadingSpinner from "../../components/Spinner";
 import NotificationBanner from "../../components/NotificationBanner";
 import KeyFacts from "./KeyFacts";
-
-import { GlobalContext } from "../../providers";
 
 const Summary = ({ data, csrf_token }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [summary, setSummary] = useState(data.summary);
   const [notification, setNotification] = useState(null);
-  const [isUpdating, setIsUpdating] = useState(false);
-  const [isCreating, setIsCreating] = useState(false);
-
-  const { isAccountManager } = useContext(GlobalContext);
-
-  const ENDPOINT = `/api/v1/company/${data.duns_number}`;
-
-  const onSubmit = async (payload) => {
-    setIsLoading(true);
-
-    const { data, status } = await ApiProxy.update(
-      ENDPOINT,
-      payload,
-      csrf_token
-    );
-
-    setIsLoading(false);
-    setIsCreating(false);
-    setIsUpdating(false);
-
-    if (status === 200) {
-      setSummary(data.data.summary);
-      setNotification({ message: "Summary updated" });
-    } else {
-      setNotification({
-        message: `Status ${status}: ${data.message || data.error}`,
-        status: "warning",
-      });
-    }
-  };
 
   return (
     <LoadingSpinner isLoading={isLoading}>
@@ -59,7 +25,7 @@ const Summary = ({ data, csrf_token }) => {
             Currently there is no summary for this company.
           </p>
         ) : (
-          !isCreating && !isUpdating && <p className="govuk-body">{summary}</p>
+         <p className="govuk-body">{summary}</p>
         )}
         <KeyFacts data={data} />
       </Section>
