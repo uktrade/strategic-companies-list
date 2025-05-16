ab2775175a8dec8702ca1e32f49fc4f0d8747efa
James Robinson <70377025+JamesPRobinson@users.noreply.github.com>
2025-05-16 14:25:15 +0100

    typo

--- scl/core/static/react/components/Section.jsx
+++ scl/core/static/react/components/Section.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Section = ({ children, title, className = '', caption='', captionStyle={color: "#505A5F", marginBottom: '20px'}}) => (
+const Section = ({ children, title, className = '', caption='', captionStyle={color: '#505A5F', marginBottom: '20px'}}) => (
   <section className={`section ${className}`}>
     <h2 className="govuk-heading-m" style={{marginBottom: '15px'}}>{title}</h2>
     {caption ? <p className="govuk-body" style={captionStyle}>{caption}</p> : <></>}
