38f71d96597d3dd477760f11022522e9f4c197ef
Edie Pearce <e.pearce@tutanota.com>
2025-05-21 14:34:46 +0100

    Update note form styling

--- scl/core/static/react/forms/notes/Create.jsx
+++ scl/core/static/react/forms/notes/Create.jsx
@@ -28,6 +28,17 @@ const Create = ({
 
   setValue("contents", transcript + " " + partialTranscript);
 
+  const errorList = function () {
+    if (errors) {
+      return (
+        <p className="govuk-error-message">
+          <span className="govuk-visually-hidden">Error:</span>
+          {errors?.contents?.message}
+        </p>
+      )
+    }
+  }
+
   return (
     <>
       <h2 className="govuk-heading-m">Add note</h2>
@@ -35,25 +46,26 @@ const Create = ({
         onSubmit={handleSubmit((data) => onSubmit(data, "create"))}
         className="scl-inine-form"
       >
-        <label className="govuk-label" htmlFor="contents">
-          Contents
-        </label>
-        {errors && (
-          <p className="govuk-error-message">
-            <span className="govuk-visually-hidden">Error:</span>
-            {errors?.contents?.message}
-          </p>
-        )}
-        {isAWSTranscribeActive && (
-          <div style={{ display: "flex", justifyContent: "flex-end" }}>
-            <TranscriptButton
-              className="govuk-!-margin-bottom-2"
-              onClick={handleOnTranscribe}
-              isTranscribing={isTranscribing}
-              disabled={isTranscribing && !hasFinalisedTranscription}
-            />
+        <div class="govuk-grid-row">
+          <div class="govuk-grid-column-three-quarters">
+            <label className="govuk-label govuk-!-font-weight-bold" htmlFor="contents">
+              Add business intelligence
+            </label>
           </div>
-        )}
+          <div class="govuk-grid-column-one-quarter">
+            {isAWSTranscribeActive && (
+              <div style={{ float: "right" }}>
+                <TranscriptButton
+                  className="govuk-!-margin-bottom-2"
+                  onClick={handleOnTranscribe}
+                  isTranscribing={isTranscribing}
+                  disabled={isTranscribing && !hasFinalisedTranscription}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        {errorList}
         <textarea
           className="govuk-textarea govuk-!-margin-bottom-4"
           id="contents"
@@ -64,6 +76,8 @@ const Create = ({
             validate: (value) => value.trim() !== "" || "Contents are required",
           })}
         ></textarea>
+
+        <hr class="govuk-!-margin-top-5 govuk-!-margin-bottom-5" />
 
         <div className="govuk-!-margin-top-2">
           <button
